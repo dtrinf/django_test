f263c2a7cb41b1300380dda7fd3efbe9d2e1f4bc
David Trigo <david.trigo@gmail.com>
2019-05-15 20:09:41 +0200

    Comments added to Blog

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from .models import Post
-#Formulario para crear los nuevos posts.
-from .forms import PostForm
+#Formulario para crear los nuevos posts y comentarios
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 #Module to authorize
 from django.contrib.auth.decorators import login_required
@@ -63,3 +63,16 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
