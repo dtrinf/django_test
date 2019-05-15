58c404d7da8df94b1182edcbc95639eb90f61d0d
David Trigo <david.trigo@gmail.com>
2019-05-15 23:08:06 +0200

    Comment approval

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
@@ -25,7 +28,7 @@ class Comment(models.Model):
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        selv.approved_comment = True
+        self.approved_comment = True
         self.save()
 
     def __str__(self):
